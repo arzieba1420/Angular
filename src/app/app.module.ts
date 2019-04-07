6fb981e5df142315df31fdd608eb00f3c771c0f3
arzieba1420 <44318275+arzieba1420@users.noreply.github.com>
2019-04-07 11:56:21 +0200

    Create path Router to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HeaderComponent} from "./header/header.component";
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const appRoutes: Routes= [
+  {path: 'contact',component: ContactComponent},
+  {path: 'home', component: HomeComponent}
+  ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
